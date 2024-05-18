77ce757cce4ee1c01a8b52c7b8edb4c0a2ebe956
AristosKass <ariskass@csd.auth.gr>
2024-05-18 10:55:14 +0000

    εφτιαξα τη main

--- main.cpp
+++ main.cpp
@@ -26,13 +26,40 @@ int main() {
   ifstream inf; // Δηλώνω ένα ρεύμα εισόδου inf
   ofstream outf; // Δηλώνω ένα ρεύμα εξόδου outf
   char s[50];
+  char s1[50]="BUILD MINHEAP filename";
+  char s2[50]="BUILD MAXHEAP filename";
+  char s3[50]="BUILD AVLTREE filename";
+  char s4[50]="BUILD GRAPH filename";
+  char s5[50]="BUILD HASHTABLE filename";
+  char s6[50]="GETSIZE MINHEAP";
+  char s7[50]="GETSIZE MAXHEAP";
+  char s8[50]="GETSIZE AVLTREE";
+  char s9[50]="GETSIZE GRAPH";
+  char s10[50]="GETSIZE HASHTABLE";
+  char s11[50]="FINDMIN MINHEAP";
+  char s12[50]="FINDMAX MAXHEAP";
+  char s13[50]="FINDMIN AVLTREE";
+  char s14[50]="SEARCH AVLTREE number" ;
+  char s15[50]="SEARCH HASHTABLE number";
+  char s16[50]="COMPUTESHORTESTPATH GRAPH number1 number2";
+  char s17[50]="COMPUTESPANNINGTREE GRAPH";
+  char s18[50]="FINDCONNECTEDCOMPONENTS GRAPH";
+  char s19[50]="INSERT MINHEAP number";
+  char s20[50]="INSERT MAXHEAP number";
+  char s21[50]="INSERT AVLTREE number";
+  char s22[50]="INSERT HASHTABLE number";
+  char s23[50]="INSERT GRAPH number1 number 2";
+  char s24[50]="DELETEMIN MINHEAP";
+  char s25[50]="DELETEMAX MAXHEAP";
+  char s26[50]="DELETE AVLTREE number";
+  char s27[50]="DELETE GRAPH number1 number2";
   inf.open("commands.txt"); // Άνοιγμα αρχείου "commands.txt" για ανάγνωση
   outf.open("output.txt"); // Άνοιγμα αρχείου "output.txt" για εγγραφή
   if (inf.is_open() && outf.is_open())
   {
     while (inf.getline(s, 50))
       {
-        if (strcmp(s, "BUILD MINHEAP filename")==0)
+        if (strcmp(s, s1)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           Minheap minh(filename1);
@@ -40,7 +67,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο σωρός ελαχίστων χτίστηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "BUILD MAXHEAP filename")==0)
+        else if (strcmp(s, s2)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           Maxheap maxh(filename2);
@@ -48,7 +75,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο σωρός μεγίστων χτίστηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "BUILD AVLTREE filename")==0)
+        else if (strcmp(s, s3)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           Avltree avlt(filename3);
@@ -56,11 +83,11 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Το δυαδικό δένδρο αναζήτησης χτίστηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "BUILD GRAPH filename")==0)
+        else if (strcmp(s, s4)==0)
         {
           
         }
-        else if (strcmp(s, "BUILD HASHTABLE filename")==0)
+        else if (strcmp(s, s5)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           Hashtable hash(filename5);
@@ -68,7 +95,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο πίνακας κατακερματισμού χτίστηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "GETSIZE MINHEAP")==0)
+        else if (strcmp(s, s6)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το πλήθος των στοιχείων του σωρού ελαχίστων είναι " << minh.getSize();
@@ -76,7 +103,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "GETSIZE MAXHEAP")==0)
+        else if (strcmp(s, s7)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το πλήθος των στοιχείων του σωρού μεγίστων είναι " << maxh.getSize();
@@ -84,7 +111,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "GETSIZE AVLTREE")==0)
+        else if (strcmp(s, s8)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το πλήθος των στοιχείων του δένδρου είναι " << avlt.getSize();
@@ -92,11 +119,11 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "GETSIZE GRAPH")==0)
+        else if (strcmp(s, s9)==0)
         {
           
         }
-        else if (strcmp(s, "GETSIZE HASHTABLE")==0)
+        else if (strcmp(s, s10)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το πλήθος των στοιχείων του πίνακα είναι " << hash.getSize();
@@ -104,7 +131,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "FINDMIN MINHEAP")==0)
+        else if (strcmp(s, s11)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το ελάχιστο στοιχείο από το σωρό ελαχίστων είναι " << minh.findMin();
@@ -112,7 +139,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "FINDMAX MAXHEAP")==0)
+        else if (strcmp(s, s12)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το μέγιστο στοιχείο από το σωρό μεγίστων είναι " << maxh.findMax();
@@ -120,7 +147,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "FINDMIN AVLTREE")==0)
+        else if (strcmp(s, s13)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           outf << "Το ελάχιστο στοιχείο από το δένδρο είναι " << avlt.findMin();
@@ -128,7 +155,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << " το οποίο ανακτήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "SEARCH AVLTREE number")==0)
+        else if (strcmp(s, s14)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           avlt.search(12);
@@ -136,7 +163,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο αριθμός αναζητήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "SEARCH HASHTABLE number")==0)
+        else if (strcmp(s, s15)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           hash.search(12);
@@ -144,19 +171,19 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο αριθμός αναζητήθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "COMPUTESHORTESTPATH GRAPH number1 number2")==0)
+        else if (strcmp(s, s16)==0)
         {
           
         }
-        else if (strcmp(s, "COMPUTESPANNINGTREE GRAPH")==0)
+        else if (strcmp(s, s17)==0)
         {
           
         }
-        else if (strcmp(s, "FINDCONNECTEDCOMPONENTS GRAPH")==0)
+        else if (strcmp(s, s18)==0)
         {
           
         }
-        else if (strcmp(s, "INSERT MINHEAP number")==0)
+        else if (strcmp(s, s19)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           minh.insert(10);
@@ -164,7 +191,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο αριθμός εισάχθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "INSERT MAXHEAP number")==0)
+        else if (strcmp(s, s20)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           maxh.insert(10);
@@ -172,7 +199,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο αριθμός εισάχθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "INSERT AVLTREE number")==0)
+        else if (strcmp(s, s21)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           avlt.insert(10);
@@ -180,7 +207,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο αριθμός εισάχθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "INSERT HASHTABLE number")==0)
+        else if (strcmp(s, s22)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           hash.insert(10);
@@ -188,11 +215,11 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο αριθμός εισάχθηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "INSERT GRAPH number1 number 2")==0)
+        else if (strcmp(s, s23)==0)
         {
           
         }
-        else if (strcmp(s, "DELETEMIN MINHEAP")==0)
+        else if (strcmp(s, s24)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           minh.deleteMin();
@@ -200,7 +227,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο ελάχιστος διαγράφτηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "DELETEMAX MAXHEAP ")==0)
+        else if (strcmp(s, s25)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           maxh.deleteMax();
@@ -208,7 +235,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Ο μέγιστος διαγράφτηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "DELETE AVLTREE number")==0)
+        else if (strcmp(s, s26)==0)
         {
           auto start = std::chrono::high_resolution_clock::now();
           avlt.deleteAvl(10);
@@ -216,7 +243,7 @@ int main() {
           std::chrono::duration<double> diff = end - start;
           outf << "Το στοιχείο διαγράφτηκε σε " << diff.count() << " δευτερόλεπτα" << endl;
         }
-        else if (strcmp(s, "DELETE GRAPH number1 number2")==0)
+        else if (strcmp(s, s27)==0)
         {
           
         }
